39e7b4f2536650b4c57e2fcde33e312957641499
unknown <reluxa@reluxapc.(none)>
2013-10-27 08:15:50 +0100

    firs working version

---
+++ src/main/java/com/vaadin/cdi/example/util/CounterService.java
@@ -0,0 +1,21 @@
+package com.vaadin.cdi.example.util;
+
+import java.io.Serializable;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.vaadin.cdi.UIScoped;
+
+@UIScoped
+public class CounterService implements Serializable {
+
+    private final AtomicInteger COUNTER = new AtomicInteger(0);
+
+    public int next() {
+        return COUNTER.incrementAndGet();
+    }
+
+    public int get() {
+        return COUNTER.get();
+    }
+
+}
