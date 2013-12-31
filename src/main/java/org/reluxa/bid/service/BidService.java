5a3c1545ef15912ee710bb449a16ee2560255436
reluxa <reluxa@gmail.com>
2013-12-31 19:10:36 +0100

    bid delete fixed

--- src/main/java/org/reluxa/bid/service/BidService.java
+++ src/main/java/org/reluxa/bid/service/BidService.java
@@ -15,6 +15,7 @@ public class BidService extends AbstractService {
 
 	public void createBid(Bid bid) {
 		db.store(bid);
+		bid.setId(db.ext().getID(bid));
 		BidModelChanged created = new BidModelChanged();
 		created.setCreated(bid);
 		beanManager.fireEvent(created);
@@ -26,15 +27,22 @@ public class BidService extends AbstractService {
 	}
 
 	public void delete(@Observes DeleteBidEvent deleteBidEvent) {
-		System.out.println("Bid delete was called..");
-		ObjectSet<Bid> bids = db.queryByExample(deleteBidEvent.getBid());
-		if (bids.size() != 1) {
-			throw new RuntimeException("Invalid object for delete:" + deleteBidEvent.getBid());
-		}
-		db.delete(bids.get(0));
+		//Bid bid2 = db.ext().getByID(deleteBidEvent.getBid().getId());
+		//System.out.println(bid2);
+		
+		
+		Bid bid = deleteBidEvent.getBid();
+		db.ext().bind(bid, bid.getId());
+		db.delete(bid);
+//		System.out.println("Bid delete was called..");
+//		ObjectSet<Bid> bids = db.queryByExample(deleteBidEvent.getBid());
+//		if (bids.size() != 1) {
+//			throw new RuntimeException("Invalid object for delete:" + deleteBidEvent.getBid());
+//		}
+//		db.delete(bids.get(0));
 
 		BidModelChanged mchanged = new BidModelChanged();
-		mchanged.setDeleted(new Bid[] { bids.get(0) });
+		mchanged.setDeleted(new Bid[] {bid});
 		beanManager.fireEvent(mchanged);
 	}
 
